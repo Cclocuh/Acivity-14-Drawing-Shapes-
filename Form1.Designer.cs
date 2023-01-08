a6c9a5d7662f5445843b8f8282a482c88e993d83
Cclocuh <118848644+Cclocuh@users.noreply.github.com>
2023-01-08 11:58:36 -0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace Activity_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawPolygons = new System.Windows.Forms.Button();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.shapeListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DrawPolygons
+            // 
+            this.DrawPolygons.Location = new System.Drawing.Point(361, 473);
+            this.DrawPolygons.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DrawPolygons.Name = "DrawPolygons";
+            this.DrawPolygons.Size = new System.Drawing.Size(109, 27);
+            this.DrawPolygons.TabIndex = 0;
+            this.DrawPolygons.Text = "Draw";
+            this.DrawPolygons.UseVisualStyleBackColor = true;
+            this.DrawPolygons.Click += new System.EventHandler(this.DrawPolygons_Click);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Location = new System.Drawing.Point(490, 473);
+            this.Clearbutton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(113, 27);
+            this.Clearbutton.TabIndex = 1;
+            this.Clearbutton.Text = "Clear";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 352);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(213, 148);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select details";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(23, 64);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(49, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Date";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 25);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(54, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Name";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Location = new System.Drawing.Point(19, 207);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(213, 132);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 75);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Outline";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 33);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fill";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // shapeListBox
+            // 
+            this.shapeListBox.FormattingEnabled = true;
+            this.shapeListBox.Items.AddRange(new object[] {
+            "Circle",
+            "Triangle",
+            "Rectangle",
+            "Pentagon"});
+            this.shapeListBox.Location = new System.Drawing.Point(19, 75);
+            this.shapeListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.shapeListBox.Name = "shapeListBox";
+            this.shapeListBox.Size = new System.Drawing.Size(204, 108);
+            this.shapeListBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Select a shape";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 580);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.shapeListBox);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.DrawPolygons);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawPolygons;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.ListBox shapeListBox;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+
+namespace Activity_14
+{ 
+    public partial class Form1 : Form
+    {
+   
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Drawbutton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void DrawPolygons_Click(object sender, EventArgs e)
+        {
+            SolidBrush sb = new SolidBrush(Color.Black);
+            Pen blackPen = new Pen(Color.Black, 3);
+            Graphics g = this.CreateGraphics();
+
+
+            string selected = shapeListBox.SelectedItem.ToString();
+            Pen p = new Pen(Color.Black, 2);
+
+            if (selected == "Rectangle")
+            {
+                Rectangle shape = new Rectangle(500, 100, 200, 100);
+                g.DrawRectangle(p, shape);
+
+                if (checkBox1.Checked)
+                {
+                    MessageBox.Show("Rectangle", "01/08/2023");
+                }
+            }
+            if(selected == "Circle")
+            {
+                g.DrawEllipse(p, 500, 100, 200, 200);
+
+                if (checkBox1.Checked)
+                {
+                    MessageBox.Show("Circle", "01/08/2023");
+                }
+                
+            }
+            else if (selected == "Triangle")
+            {
+                Point point1 = new Point(350, 100);
+                Point point2 = new Point(300, 200);
+                Point point3 = new Point(400, 200);
+                Point[] triangle = { point1, point2, point3 };
+                g.DrawPolygon(p, triangle);
+
+                if (checkBox1.Checked)
+                {
+                    MessageBox.Show("Triangle", "01/08/2023");
+                }
+            } 
+            else if (selected == "Pentagon")
+            {
+                Point point1 = new Point(270, 315);
+                Point point2 = new Point(350, 260);
+                Point point3 = new Point(425, 315);
+                Point point4 = new Point(400, 400);
+                Point point5 = new Point(300, 400);
+                Point[] pentagon = { point1, point2, point3, point4, point5 };
+                g.DrawPolygon(p, pentagon);
+
+                if (checkBox1.Checked)
+                {
+                    MessageBox.Show("Pentagon", "01/08/2023");
+                }
+            }
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            Clearbutton.FindForm().Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
